eb97e5e3c5a779b3fd4d1773db2d7cd88b9f5385
Carlos Eduardo Hilario Ferreira <eduardohilarioferreira@gmail.com>
2024-12-20 21:04:09 -0300

    Arrumando estrutura, settings, criando novo app

--- inventory_system/apps.py
+++ inventory_system/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GteConfig(AppConfig):
+class InventorySystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GTE'
+    name = 'inventory_system'
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.GTE'
+    'inventory_system'
 ]
 
 THIRD_APPS = [
