87581914460451993dae43c482ac1b1d1a67e2bc
Carlos Eduardo Hilario Ferreira <eduardohilarioferreira@gmail.com>
2024-12-20 21:08:42 -0300

    Testando arquivos globais

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="Pagina 01"),
+    path('paginaTeste/', views.home, name="Pagina Teste"),
 ]
--- inventory_system/views.py
+++ inventory_system/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse
 
 
+def paginaTeste(request):
+    return render(request, "global/index.html")
+
+
 def home(request):
     return HttpResponse("Ola")
